4801959e97b32fa6fd756691ac8fd9e85ab220e7
kaaboaye <kaaboaye@gmail.com>
2020-04-02 17:24:14 +0200

    refactor bigints

--- src/helpers/bigint-validation.ts
+++ src/helpers/bigint-validation.ts
@@ -1,21 +1,22 @@
 import Joi from "@hapi/joi";
 
+const bigintJoi = Joi.extend({
+  type: "bigint",
+  coerce: {
+    from: "string",
+    method(value, { error }) {
+      try {
+        return { value: BigInt(value) };
+      } catch (err) {
+        return { errors: [error("bigint.err", err)] };
+      }
+    }
+  },
+  messages: {
+    "bigint.err": "{{#label}} {{#message}}"
+  }
+});
+
 // Here it's better to infer return type
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const BigIntValidation = () =>
-  Joi.extend({
-    type: "bigint",
-    coerce: {
-      from: "string",
-      method(value, { error }) {
-        try {
-          return { value: BigInt(value) };
-        } catch (err) {
-          return { errors: [error("bigint.err", err)] };
-        }
-      }
-    },
-    messages: {
-      "bigint.err": "{{#label}} {{#message}}"
-    }
-  }).bigint();
+export const BigIntValidation = () => bigintJoi.bigint();
