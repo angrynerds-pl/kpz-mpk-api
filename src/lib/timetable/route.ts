690c168b1996ecd556cc35ef4fede77e8ce5e9ee
kaaboaye <kaaboaye@gmail.com>
2020-04-22 23:00:09 +0200

    move route type to it's own file

--- src/lib/timetable/route.ts
+++ src/lib/timetable/route.ts
@@ -1,17 +1,5 @@
 import { Entity, PrimaryColumn, Column, Check } from "typeorm";
-
-export enum TimetableRouteType {
-  Tram = 0,
-  Subway = 1,
-  Rail = 2,
-  Bus = 3,
-  Ferry = 4,
-  CableTram = 5,
-  AerialLift = 6,
-  Funicular = 7,
-  Trolleybus = 11,
-  Monorail = 12
-}
+import { TimetableRouteType } from "./route-type";
 
 @Entity({ name: "timetable_route" })
 export class TimetableRoute {
