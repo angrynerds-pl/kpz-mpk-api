6a2cab37f204f8e14ac5b3ef7afd1d8f80a3903e
kaaboaye <kaaboaye@gmail.com>
2020-04-22 22:58:55 +0200

    reformat create line_lookup view

--- src/migrations/1587235011629-DefineLineLookupView.ts
+++ src/migrations/1587235011629-DefineLineLookupView.ts
@@ -4,29 +4,29 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class DefineLineLookupView1587235011629 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-                create materialized view line_lookup as
-                select s.line,
-                      t.route_id,
-                      t.trip_headsign
-                from (
-                      select shape_id,
-                              st_makeline(
-                                ST_SetSRID(st_makepoint(
+                CREATE MATERIALIZED VIEW line_lookup AS
+                SELECT s.line,
+                       t.route_id,
+                       t.trip_headsign
+                FROM (
+                      SELECT shape_id,
+                              ST_MakeLine(
+                                ST_SetSRID(ST_MakePoint(
                                             s.shape_pt_lon,
                                             s.shape_pt_lat
                                             ), 4326),
-                                ST_SetSRID(st_makepoint(
-                                              lead(s.shape_pt_lon) over (partition by s.shape_id order by s.shape_pt_sequence),
-                                              lead(s.shape_pt_lat) over (partition by s.shape_id order by s.shape_pt_sequence)
+                                ST_SetSRID(ST_MakePoint(
+                                              lead(s.shape_pt_lon) OVER (partition by s.shape_id order by s.shape_pt_sequence),
+                                              lead(s.shape_pt_lat) OVER (partition by s.shape_id order by s.shape_pt_sequence)
                                             ), 4326)
                                 ) line
-                      from timetable_shape s
+                      FROM timetable_shape s
                     ) s
-                left join timetable_trips t on s.shape_id = t.shape_id
-                where line is not null
-                group by s.line,
-                        t.route_id,
-                        t.trip_headsign
+                LEFT JOIN timetable_trips t on s.shape_id = t.shape_id
+                WHERE line IS NOT NULL
+                GROUP BY s.line,
+                         t.route_id,
+                         t.trip_headsign
         `);
   }
 
