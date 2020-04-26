a51d4f5dd981dd948d7dd528677e72fa2fde63c1
kaaboaye <kaaboaye@gmail.com>
2020-04-26 20:05:59 +0200

    enable postgis extension

---
+++ src/migrations/1587235010629-EnablePostgis.ts
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class EnablePostgis1587235010629 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+          CREATE EXTENSION IF NOT EXISTS postgis;
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+           DROP EXTENSION IF EXISTS postgis;
+    `);
+  }
+}
